bc52ae27bea238ab1e80d6b7ecf40d5b1d3713d4
Huzaifa_Habib <cocobutter128@gmail.com>
2023-02-22 12:15:29 +0500

    WEB fOR ME

---
+++ oject.js
@@ -0,0 +1,41 @@
+// let cricketerDetails = {
+//     cricketerName : "Hasan" ,
+//     age : 19 ,
+//     gender : "male",
+//     address : "Al-Noor Society",
+//     mobileNumber :'03170243866',
+//     password : "hasssuash"
+// }
+// console.log(cricketerDetails);
+
+// const userMobileNumber =prompt ("chal by mobile number dy")
+// const userPassword =prompt("chal by password dy")
+
+// const detailsOfUser ={
+//     mobileNumber : userMobileNumber,
+//     password :userPassword 
+// }
+// // console.log(cricketerDetails[mobileNumber]);
+
+// if(cricketerDetails.mobileNumber==detailsOfUser.mobileNumber){
+//     if(cricketerDetails.password==detailsOfUser.password){
+//         alert("You haved logged in")
+//     }else{
+//         alert("password is incorrect")
+//     }
+
+// }else{
+//     alert("User not found")
+// }
+
+// const details = {
+//     firstName: "Muhammad",
+//     lastName: "Hasan",
+//     fullName: function () {
+//         return this.firstName +" " + this.lastName
+//     }
+// }
+
+
+
+// alert(details.fullName())
